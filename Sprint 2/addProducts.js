a378e56330f38044c09c76a450ef634edfea26d2
anneliszt <aldestajo@up.edu.ph>
2022-06-04 11:29:06 +0800

    Updated sprint 2

---
+++ Sprint 2/addProducts.js
@@ -0,0 +1,14 @@
+// Allows file uploads
+
+function triggerClick(e) {
+    document.querySelector('#productImage').click();
+  }
+  function displayImage(e) {
+    if (e.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e){
+        document.querySelector('#productDisplay').setAttribute('src', e.target.result);
+      }
+      reader.readAsDataURL(e.files[0]);
+    }
+  }
